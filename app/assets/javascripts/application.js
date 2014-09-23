1ddfd6cf8e78d7312197f97242208a4fd029d40a
Jlit1001 <jordanlittell@yahoo.com>
2014-09-23 10:01:46 -0700

    assets again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,10 +16,10 @@
 //= require underscore
 //= require backbone
 //= require trak_my_run
-//= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
+//= require_tree .
 //= require jquery
 
