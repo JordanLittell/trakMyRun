d55d6de9c5f945a55c4a8b9cc33e75defe4ea23e
Jlit1001 <jordanlittell@yahoo.com>
2014-09-23 10:12:10 -0700

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :debug
+  config.assets.initialize_on_precompile = false
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
