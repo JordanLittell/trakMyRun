718eff9ca414d7af5504e42290b0d0eedab0d536
Jlit1001 <jordanlittell@yahoo.com>
2014-09-23 10:23:08 -0700

    modified js manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,6 @@
 //= require underscore
 //= require backbone
 //= require trak_my_run
-//= require_tree ./models
-//= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
