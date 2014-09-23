19715f6ce6432324a62b4b545363bcd8eed08557
Jlit1001 <jordanlittell@yahoo.com>
2014-09-23 00:52:04 -0700

    generated backbone scaffolding

---
+++ app/assets/javascripts/trak_my_run.js
@@ -0,0 +1,13 @@
+window.TrakMyRun = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  TrakMyRun.initialize();
+});
--- app/assets/javascripts/trak_my_run.js.coffee
+++
@@ -1,9 +0,0 @@
-window.TrakMyRun =
-  Models: {}
-  Collections: {}
-  Views: {}
-  Routers: {}
-  initialize: -> alert 'Hello from Backbone!'
-
-$(document).ready ->
-  TrakMyRun.initialize()
